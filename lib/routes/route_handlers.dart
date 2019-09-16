03f1a19e4a80bb17c2d3fb3ad18725f1656ecdaf
Leon <lw75251@gmail.com>
2019-09-16 18:21:01 -0400

    Adds Fluro Routing and Fonts

---
+++ lib/routes/route_handlers.dart
@@ -0,0 +1,8 @@
+import 'package:fluro/fluro.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+Handler welcomeHandler = Handler(
+  handlerFunc: (BuildContext context, Map<String, List<String>> params) {
+    return null;
+  });
---
+++ lib/routes/router.dart
@@ -0,0 +1,38 @@
+// import 'package:flutter/material.dart';
+// import 'package:friendly/routes/undefined_view.dart';
+// import 'package:friendly/ui/gradients.dart';
+// import 'package:friendly/ui/login/landing.dart';
+// import 'package:friendly/ui/login/slides.dart';
+
+// const String LandingScreenRoute = '/';
+// const String SignUpRoute = 'signup';
+
+// class Router {
+  
+
+//   static Route<dynamic> generateRoute(RouteSettings settings) {
+//     switch (settings.name) {
+//       case LandingScreenRoute:
+//         return MaterialPageRoute(builder: (_) => LandingScreen());
+//       case SignUpRoute:
+//         return MaterialPageRoute(builder: (context) => FriendlySwiper());
+//       default:
+//         return MaterialPageRoute(builder: (context) => UndefinedView(name: settings.name,));
+//     }
+//   }
+// }
+
+import 'package:fluro/fluro.dart';
+import 'package:mental_math/routes/route_handlers.dart';
+
+
+const String welcomeRoute = '/';
+const String signUpRoute = '/signup';
+
+final router = Router();
+
+class Routes {
+  static void defineRoutes(Router router) {
+    router.define(welcomeRoute, handler: welcomeHandler, transitionType: TransitionType.fadeIn);
+  }
+}
